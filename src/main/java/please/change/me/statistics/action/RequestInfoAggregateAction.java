d91c7274eb70f2a299dccce30a98caff7142fc6d
sukegawatakashitis <110877073+sukegawatakashitis@users.noreply.github.com>
2023-03-10 15:19:54 +0900

    refactor: SonarQube指摘対応 (#9)
    * 文字列を定数化しました。
    
    * parseIntを使用するよう修正しました。
    
    * 文字列を定数化しました。

--- src/main/java/please/change/me/statistics/action/RequestInfoAggregateAction.java
+++ src/main/java/please/change/me/statistics/action/RequestInfoAggregateAction.java
@@ -62,6 +62,30 @@ public class RequestInfoAggregateAction extends BatchAction<DataRecord> {
     /** 年月単位のリクエスト情報ファイルのファイル名のプレフィックス */
     private static final String REQUEST_INFO_SUMMARY_YM_PREFIX = "REQUEST_INFO_SUMMARY_YM_";
 
+    /** リクエストID */
+    private static final String REQUEST_ID = "requestId";
+
+    /** 集計単位値 */
+    private static final String AGGREGATE_UNIT_VALUE = "aggregateUnitValue";
+
+    /** プロセス名 */
+    private static final String PROCESS_NAME = "processName";
+
+    /** 処理リクエスト数 */
+    private static final String REQUEST_COUNT = "requestCount";
+
+    /** 処理時間が閾値を超えたリクエスト数 */
+    private static final String THRESHOLD_OVER_COUNT = "thresholdOverCount";
+
+    /** 処理時間（平均） */
+    private static final String AVERAGE = "average";
+
+    /** 処理時間（中央値） */
+    private static final String MEDIAN = "median";
+
+    /** 処理時間（最大値） */
+    private static final String MAX = "max";
+
     /** 日、時間単位の集計結果を保持するMapオブジェクト */
     private final Map<AggregateKey, Aggregator> aggregateResultHolder = new TreeMap<AggregateKey, Aggregator>();
 
@@ -102,7 +126,7 @@ public class RequestInfoAggregateAction extends BatchAction<DataRecord> {
 
         if (systemDateYm.equals(ymStr)) {
             // システム日付と同月のデータのみ年月集計を行う。
-            int ym = Integer.valueOf(ymStr);
+            int ym = Integer.parseInt(ymStr);
             addRequestInfo(ymAggregateResultHolder, inputData, AggregateUnit.YEAR_MONTH, ym);
         }
 
@@ -130,12 +154,12 @@ public class RequestInfoAggregateAction extends BatchAction<DataRecord> {
             int unitValue) {
 
         // プロセス名を保持する。
-        String processName = inputData.getString("processName");
+        String processName = inputData.getString(PROCESS_NAME);
         processNames.add(processName);
 
         // 集計単位のキー
         AggregateKey key = new AggregateKey(
-                inputData.getString("requestId"),
+                inputData.getString(REQUEST_ID),
                 processName,
                 aggregateUnit, unitValue);
 
@@ -204,14 +228,14 @@ public class RequestInfoAggregateAction extends BatchAction<DataRecord> {
 
         // CSVタイトルの出力
         Map<String, String> title = new HashMap<String, String>();
-        title.put("requestId", "リクエストID");
-        title.put("aggregateUnitValue", aggregateUnitValue);
-        title.put("processName", "プロセス名");
-        title.put("requestCount", "処理リクエスト数");
-        title.put("thresholdOverCount", "処理時間が閾値を超えたリクエスト数");
-        title.put("average", "処理時間（平均）");
-        title.put("median", "処理時間（中央値）");
-        title.put("max", "処理時間（最大値）");
+        title.put(REQUEST_ID, "リクエストID");
+        title.put(AGGREGATE_UNIT_VALUE, aggregateUnitValue);
+        title.put(PROCESS_NAME, "プロセス名");
+        title.put(REQUEST_COUNT, "処理リクエスト数");
+        title.put(THRESHOLD_OVER_COUNT, "処理時間が閾値を超えたリクエスト数");
+        title.put(AVERAGE, "処理時間（平均）");
+        title.put(MEDIAN, "処理時間（中央値）");
+        title.put(MAX, "処理時間（最大値）");
         FileRecordWriterHolder.write(title, logParseDefinition.getRequestInfoSummaryBaseName(), fileName);
     }
 
@@ -225,14 +249,14 @@ public class RequestInfoAggregateAction extends BatchAction<DataRecord> {
             Aggregator aggregator) {
 
         Map<String, Object> data = new HashMap<String, Object>();
-        data.put("requestId", aggregateKey.requestId);
-        data.put("aggregateUnitValue", aggregateKey.aggregateUnitValue);
-        data.put("processName", aggregateKey.processName);
-        data.put("requestCount", aggregator.requestCount);
-        data.put("thresholdOverCount", aggregator.thresholdOverCount);
-        data.put("average", aggregator.getAverageTime());
-        data.put("max", aggregator.maxExecutionTime);
-        data.put("median", aggregator.getMedianTime());
+        data.put(REQUEST_ID, aggregateKey.requestId);
+        data.put(AGGREGATE_UNIT_VALUE, aggregateKey.aggregateUnitValue);
+        data.put(PROCESS_NAME, aggregateKey.processName);
+        data.put(REQUEST_COUNT, aggregator.requestCount);
+        data.put(THRESHOLD_OVER_COUNT, aggregator.thresholdOverCount);
+        data.put(AVERAGE, aggregator.getAverageTime());
+        data.put(MAX, aggregator.maxExecutionTime);
+        data.put(MEDIAN, aggregator.getMedianTime());
 
         // 集計単位を元に、出力ファイルを切り替えて集計結果を出力する。
         String fileName = null;
